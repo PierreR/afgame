2fd4e9fe8ae0b33cf383fe93c6aef0f5b90c28f7
PierreR <pierrer@pi3r.be>
2013-11-11 23:39:37 +0100

    Add a few tests back

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -1,32 +1,10 @@
-{-# LANGUAGE RankNTypes #-}
 module Main 
 
 where
 
 import Pipes
-import Pipes.Lift
-import qualified Control.Monad.State.Strict as S
 import qualified Pipes.Prelude as P
 import Afgame
 
---testme :: Consumer Int IO String
---testme =  (lift $ fmap read getLine) >~ score >~ scorePrinter >~ P.stdoutLn
-
-
-
-counter :: Monad m => Pipe Char Int (S.StateT Int m) ()
-counter = go
-  where
-    go = do
-        x <- await
-        lift $ S.modify (+1)
-        r <- lift S.get
-        yield r
-        go
-
-localCounter :: Monad m => Pipe Char Int m ()
-localCounter = evalStateP 0 counter
-
 main :: IO ()
-main =  --runEffect $ each "hi there" >-> localCounter >-> P.printt
-    runEffect $ score prompt >-> P.print
+main =  runEffect $ score prompt >-> P.print
--- haskell/test/test.hs
+++ haskell/test/test.hs
@@ -3,10 +3,11 @@ where
 
 import Pipes
 import Pipes.Lift
+import qualified Pipes.Prelude as P
 import Afgame
 import Test.Framework (testGroup, defaultMain)
 import Test.Framework.Providers.HUnit
-
+import Control.Applicative
 import Test.HUnit hiding (test)
 
 -- let's make some easy shots
@@ -18,25 +19,22 @@ normal = (Normal, 5)
 main :: IO ()
 main = defaultMain tests
 
---testNormal =
---    runEffect $ for (each $ replicate 15 5) $ evalStateP emptyBoard parseShot >~ score' 
---    @?=
---    Just (take 15 $ [5, 10 ..], replicate 5 $ replicate 3 normal)
+testNormal = do
+    s <-  P.toListM $ score (each $ replicate 15 5) >-> P.map fst 
+    s @?= take 15 [5, 10 ..]
 
---testLastFrameNormal =
---    runStateT (sequence [shot 5]) [[strike], [strike], [strike], [strike]]
---    @?=
---    Just ([(60+50+35+20+5)], [ [normal], [strike],[strike], [strike], [strike] ])
+testLastFrameNormal = do
+    s <-  P.toListM $ score (each $ [15, 15, 15, 15, 5]) >-> P.map fst 
+    s @?= [15,45,90,150,170]
 
 --testGameOver =
 --    runStateT (shot 5) [[strike, strike, strike, strike], [strike], [strike], [strike], [strike]]
 --    @?=
 --    Nothing
 
---testOneStrike =
---    runStateT (shot 15) [[]]
---    @?=
---    Just (15, [[strike]])
+testOneStrike = do
+    s <- P.toListM $ (score $ yield 15)
+    s @?= [(15,[[strike]])]
 
 --testStrikeScore =
 --    runStateT (sequence [shot 15, shot 5, shot 5, shot 5, shot 5]) [[]]
@@ -84,22 +82,22 @@ testOverNormalLastFrame_2 =
     True
 
 tests = [
-    --[ testGroup "Normal"
-    --    [ testCase "Update frames with 15 normal shots" testNormal
-    --    --, testCase "one normal shots on the last frames" testLastFrameNormal
+    testGroup "Normal"
+        [ testCase "Update frames with 15 normal shots" testNormal
+        , testCase "one normal shots on the last frames" testLastFrameNormal
     --    --, testCase "Game should terminate" testGameOver
-    --    ]
-    --, testGroup "Strike"
-    --    [ testCase "Just one Strike" testOneStrike
+        ],
+    testGroup "Strike"
+       [ testCase "Just one Strike" testOneStrike
     --    , testCase "The 3 next shots are added to the score of one strike" testStrikeScore
     --    , testCase "Last frame is special" testLastFrameStrike
-    --    ]
+       ],
     --, testGroup "Spare"
     --    [ testCase "Just one Spare" testOneSpare
     --    , testCase "The 2 next shots are added to the score of a spare" testSpareScore
     --    , testCase "The 2 last shots are added to the score of the spare" testLastFrameSpare
     --    ]
-     testGroup "LastFrame"
+    testGroup "LastFrame"
         [ testCase "We should allow 4 shots in this frame" testOverLongerLastFrame_1
         , testCase "We should allow only 3 shots in this frame" testOverLongerLastFrame_2
         , testCase "We should allow only 3 shots in this frame" testOverNormalLastFrame_1
